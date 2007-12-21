2b45f27c7864789b9db1155d9cd18e4ecdbf4d2d
Robert Rothenberg <rrwo@cpan.org>
2007-12-21 15:46:36 +0000

    Import of RRWO/Graphics-ColorNames-EmergyC-1.01 from CPAN.
    gitpan-cpan-distribution: Graphics-ColorNames-EmergyC
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         RRWO/Graphics-ColorNames-EmergyC-1.01.tar.gz
    gitpan-cpan-author:       RRWO
    gitpan-cpan-maturity:     released

--- lib/Graphics/ColorNames/EmergyC.pm
+++ lib/Graphics/ColorNames/EmergyC.pm
@@ -15,10 +15,25 @@ Graphics::ColorNames::EmergyC - Eco-friendly web-design color-palette.
 
 The Emergy-C color palette is based on the
 EnergyStar wattage ratings for different colors.  Supposedly, a
-display mainly using colors of this palette uses on oaverage only
+display mainly using colors of this palette uses on average only
 about 3 or 4 watts more than a completely black screen.  White is
 included as an accent color, only for a small proportion
 of the screen.
+
+Since it is a minimal palette, you may want to use it in conjunction
+with other palettes.
+
+=begin readme
+
+=head1 REVISION HISTORY
+
+Changes since the last release
+
+=for readme include file=Changes start=^1.01 stop=^1.00 type=text
+
+More details can be found in the F<Changes> file.
+
+=end readme
 
 =head1 SEE ALSO
 
@@ -29,7 +44,6 @@ L<http://ecoiron.blogspot.com/2007/01/emergy-c-low-wattage-palette.html>.
 
 EnergyStar wattage ratings for colors,
 L<http://www.microtech.doe.gov/EnergyStar/info.htm>.
-
 
 =head1 AUTHOR
 
@@ -46,13 +60,10 @@ modify it under the same terms as Perl itself.
 
 =cut
 
-require 5.006;
 
 use strict;
 
-our $VERSION = '1.00';
-
-
+our $VERSION = '1.01';
 
 sub NamesRgbTable() {
   use integer;
